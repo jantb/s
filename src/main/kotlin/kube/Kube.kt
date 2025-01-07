454033daef9fa2dc3fb57db0c30b95fe94b8fff9
Jan Tore Bøe <jan.tore.boe@spenngroup.com>
2025-01-07 09:45:13 +0100

    Reduce memory cap and improve UI refresh rate.
    Lowered the memory capacity limit from 10,000 to 1,000 to optimize resource usage. Improved UI responsiveness by decreasing the refresh interval from 16ms to 8ms. Minor code refactors were made for better readability and efficiency.

--- src/main/kotlin/kube/Kube.kt
+++ src/main/kotlin/kube/Kube.kt
@@ -12,91 +12,97 @@ import java.io.InputStreamReader
 import java.util.concurrent.atomic.AtomicBoolean
 
 class Kube {
-  private val listenedPods: MutableMap<String, AtomicBoolean> = mutableMapOf()
+    private val listenedPods: MutableMap<String, AtomicBoolean> = mutableMapOf()
 
-  init {
-    val thread = Thread {
-      while (true) {
-        when (val msg = Channels.podsChannel.take()) {
-          is ListPods -> msg.result.complete(listPodsInAllNamespaces())
-          is ListenToPod -> {
-            listenedPods[msg.podName] = addLogsToIndex(msg.podName)
-          }
-          is UnListenToPod -> {
-            listenedPods.remove(msg.podName)!!.set(false)
-            // Channels.cmdChannel.put(ClearNamedIndex(msg.podName))
-          }
-          is UnListenToPods -> {
-            listenedPods.forEach { it.value.set(false) }
-            listenedPods.clear()
-          }
-        }
-      }
-    }
-    thread.isDaemon = true
-    thread.start()
-  }
+    init {
+        val thread = Thread {
+            while (true) {
+                when (val msg = Channels.podsChannel.take()) {
+                    is ListPods -> msg.result.complete(listPodsInAllNamespaces())
+                    is ListenToPod -> {
+                        listenedPods[msg.podName] = addLogsToIndex(msg.podName)
+                    }
 
-  val client = KubernetesClientBuilder().build()
+                    is UnListenToPod -> {
+                        listenedPods.remove(msg.podName)!!.set(false)
+                        // Channels.cmdChannel.put(ClearNamedIndex(msg.podName))
+                    }
 
-  fun listPodsInAllNamespaces(): List<PodUnit> {
-    return client.pods().inAnyNamespace().list().items.map {
-      PodUnit(
-        name = it.metadata.name,
-        version = it.status.containerStatuses.firstOrNull { it.name != "istio-proxy" && it.name != "daprd" }?.image?.substringAfterLast(":") ?: "",
-        creationTimestamp = it.metadata.creationTimestamp
-      )
-    }
-  }
-
-  private fun getLogSequence(pod: String, namespace: String): LogWatch {
-    val podResource = client.pods().inNamespace(namespace).withName(pod)
-    return podResource.inContainer(podResource.get().spec.containers.first { it.name != "istio-proxy"  && it.name != "daprd" }.name)
-      .usingTimestamps().watchLog()
-  }
-
-  private fun getLogSequencePrev(pod: String, namespace: String): List<String> {
-    val podResource = client.pods().inNamespace(namespace).withName(pod)
-    return try {
-      podResource.inContainer(podResource.get().spec.containers.first { it.name != "istio-proxy"  && it.name != "daprd" }.name)
-        .usingTimestamps().terminated().log.split("\n")
-    } catch (e: Exception) {
-      emptyList()
-    }
-  }
-
-  @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
-  private fun addLogsToIndex(pod: String): AtomicBoolean {
-    val threadDispatcher = newSingleThreadContext("CoroutineThread")
-    val scope = CoroutineScope(threadDispatcher)
-
-    val notStopping = AtomicBoolean(true)
-    scope.launch {
-      val podNamespacePair = client.pods().inAnyNamespace().list().items
-        .firstOrNull { it.metadata.name == pod }?.metadata?.namespace ?: return@launch
-
-      getLogSequencePrev(pod, podNamespacePair).forEach {
-        Channels.popChannel.send(AddToIndex(it, pod))
-      }
-
-      while (notStopping.get()) {
-        try {
-          val logSequence = getLogSequence(pod, podNamespacePair)
-          BufferedReader(InputStreamReader(logSequence.output)).use { reader ->
-            while (notStopping.get()) {
-              val line = reader.readLine()
-              if (line != null) {
-                Channels.popChannel.send(AddToIndex(line, pod))
-              }
+                    is UnListenToPods -> {
+                        listenedPods.forEach { it.value.set(false) }
+                        listenedPods.clear()
+                    }
+                }
             }
-          }
-        } catch (e: Exception) {
-           notStopping.set(false)
         }
-      }
+        thread.isDaemon = true
+        thread.start()
     }
-    return notStopping
-  }
+
+    val client = KubernetesClientBuilder().build()
+
+    fun listPodsInAllNamespaces(): List<PodUnit> {
+        return client.pods().inAnyNamespace().list().items.map {
+            PodUnit(
+                name = it.metadata.name,
+                version = it.status.containerStatuses.firstOrNull { it.name != "istio-proxy" && it.name != "daprd" }?.image?.substringAfterLast(
+                    ":"
+                ) ?: "",
+                creationTimestamp = it.metadata.creationTimestamp
+            )
+        }
+    }
+
+    private fun getLogSequence(pod: String, namespace: String): LogWatch {
+        val podResource = client.pods().inNamespace(namespace).withName(pod)
+        return podResource.inContainer(podResource.get().spec.containers.first { it.name != "istio-proxy" && it.name != "daprd" }.name)
+            .usingTimestamps().watchLog()
+    }
+
+    private fun getLogSequencePrev(pod: String, namespace: String): List<String> {
+        val podResource = client.pods().inNamespace(namespace).withName(pod)
+        return try {
+            podResource.inContainer(podResource.get().spec.containers.first { it.name != "istio-proxy" && it.name != "daprd" }.name)
+                .usingTimestamps().terminated().log.split("\n")
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+    private fun addLogsToIndex(pod: String): AtomicBoolean {
+        val threadDispatcher = newSingleThreadContext("CoroutineThread")
+        val scope = CoroutineScope(threadDispatcher)
+
+        val notStopping = AtomicBoolean(true)
+        scope.launch {
+            val podNamespacePair = client.pods().inAnyNamespace().list().items
+                .firstOrNull { it.metadata.name == pod }?.metadata?.namespace ?: return@launch
+
+            getLogSequencePrev(pod, podNamespacePair).forEach {
+                Channels.popChannel.send(AddToIndex(it, pod))
+            }
+
+            while (notStopping.get()) {
+                try {
+                    val logSequence = getLogSequence(pod, podNamespacePair)
+                    BufferedReader(InputStreamReader(logSequence.output)).use { reader ->
+                        while (notStopping.get()) {
+                            val line = reader.readLine()
+                            if (line.isNullOrBlank()) {
+                                notStopping.set(false)
+                            }
+                            Channels.popChannel.send(AddToIndex(line, pod))
+
+                        }
+                    }
+                } catch (e: Exception) {
+                    notStopping.set(false)
+                }
+            }
+        }
+        return notStopping
+    }
 }
 
 data class PodUnit(val name: String, val version: String, val creationTimestamp: String)
